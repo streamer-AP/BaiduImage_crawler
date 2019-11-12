5286d7862273d685c5f358aac5917a4fdef7e12a
streamer-AP <cryanxin@foxmail.com>
2019-11-12 11:20:15 +0800

    添加readme.md

--- BaiduImages/move_img.py
+++ BaiduImages/move_img.py
@@ -3,12 +3,12 @@ import shutil
 import json
 
 query_dict={}
-with open("BaiduImages/query_list.txt","r") as query_list_file:
+with open("query_list.txt","r") as query_list_file:
     lines=query_list_file.readlines()
     for line in lines:
         line=line.strip("\n").split(",")
         query_dict[line[0]]=line[2]
-query_json=json.load(open("BaiduImages/query.json","r"))
+query_json=json.load(open("query.json","r"))
 for json_item in query_json:
-    img_path="BaiduImages/images/"+json_item["path"]
-    shutil.move(img_path,"BaiduImages/images/"+query_dict[json_item["queryWordindex"]])
+    img_path="images/"+json_item["path"]
+    shutil.move(img_path,"images/"+query_dict[json_item["queryWordindex"]])
---
+++ readme.md
@@ -0,0 +1,31 @@
+# 基于scrapy的百度图片爬虫
+
+## 使用方法
+
+将项目clone 到本地，进入BaiduImages作为主目录
+
+使用pip安装scrapy
+
+```sh
+pip install scrapy
+```
+
+在BaiduImages/ 文件夹下修改query_list.txt，文件格式为：
+"{queryWordindex},{queryWord},{word},{pages}"，分别表示查询词id，查询词，关键词，查询页数(百度图片当前每页默认30个图片，这个页数*30基本上就是最后会下载的图片数量)
+例如: "1,大雁塔,大雁塔,30"
+
+运行爬虫，将输出保存到query.json
+
+``` sh
+scrapy crawler -o query.json
+```
+
+将图片移动到相应文件夹
+
+```sh
+python move_img.py
+```
+
+结果如下图:
+
+![](images/result.png)
